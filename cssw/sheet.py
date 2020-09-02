10efd4dc68e5d61c10e851ae4e70f8cc05de5b73
Rached Mejri <r.mejri74100@gmail.com>
2020-09-02 12:47:15 +0200

    refactor: using urllib instead of requests

--- cssw/sheet.py
+++ cssw/sheet.py
@@ -1,34 +1,30 @@
-import tempfile
+from cssw.food import Food
+from cssw.nutriment import Nutriment
 
 import xlrd
-import requests
-import xlutils
-
+from urllib import request
 
 class CohenSpreadSheet:
     def __init__(self):
-        self.tempfile = tempfile.NamedTemporaryFile()
-        self.tempfile.write(self._get_cohen_spread_sheet)
-        work_book = xlrd.open_workbook(self.tempfile.name)
+        work_book = xlrd.open_workbook(self._get_cohen_spread_sheet)
         self.number_of_leaf = work_book.sheet_names()
         self.leaf = work_book.sheet_by_name("Feuil1")
 
     @property
     def _get_cohen_spread_sheet(self) -> bytes:
         cohen_spred_sheet_url = "http://g.cohen.free.fr/telecharge/Calories.xls"
-        cohen_spred_sheet = requests.get(cohen_spred_sheet_url, stream=True)
 
-        if cohen_spred_sheet.status_code != 200:
-            raise Exception(
-                f"Can't get the document located at : {cohen_spred_sheet_url}"
-            )
+        try:
+            file_location, header = request.urlretrieve(cohen_spred_sheet_url)
+            return file_location
 
-        return cohen_spred_sheet.content
+        except Exception as why:
+            print(f"Can't get the document located at {cohen_spred_sheet_url} : {why}")        
 
-    def get_food_by_food_family(self, food_family) -> list:
+    def get_food_by_food_family(self, food_family: Food) -> list:
         return [self.leaf.cell_value(food, 0) for food in range(*food_family.value)]
 
-    def get_nutriment_by_food_family(self, nutriment, food_family) -> list:
+    def get_nutriment_by_food_family(self, nutriment: Nutriment, food_family: Food) -> list:
         nutriment_by_food = []
 
         for food_position in range(*food_family.value):
